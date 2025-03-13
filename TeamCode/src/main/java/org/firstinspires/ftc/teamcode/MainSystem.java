2bf6c1f4810487fcb62bf07a2f395a631563f991
Emilio <eramirezc145@gmail.com>
2025-03-13 17:01:06 -0600

    Added basic structure for the code and installed FTClib with its dependencies onto Gradle Build

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
 public class MainSystem extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-
+        //Declaration
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Drivetrain extends SubsystemBase{
+}
+
